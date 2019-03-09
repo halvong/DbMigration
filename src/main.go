a54dd22cffe418b073ae2014e8251443163cfb5d
Hal Vong <hvong@legalzoom.com>
2019-03-08 18:33:49 -0600

    Read file and output. Regexp works.

--- src/main.go
+++ src/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strings"
+	"io/ioutil"
 	"strconv"
 	"path/filepath"
 )
@@ -11,7 +13,8 @@ import (
 func main() {
 
 	var files []string
-	root := "/home/hal/dumps/Dump20190308"
+	//root := "/home/hal/dumps/Dump20190308"
+	root := "/home/hal/dumps/imports"
 
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
         files = append(files, path)
@@ -31,6 +34,22 @@ func main() {
 
 			if match { 
 				fmt.Println(strconv.Itoa(idx) + ". " + file) 
+
+				r, err := ioutil.ReadFile(file)
+
+				if err != nil {
+					panic(err)
+	            }
+
+				fmt.Println(r)
+
+				content := strings.Replace(string(r), "CREATE DATABASE  IF NOT EXISTS `web_main_live`","", -1)
+
+				err = ioutil.WriteFile(file, []byte(content), 0)
+				if err != nil {
+					panic(err)
+				}
+
 				idx += 1
 			}
 
