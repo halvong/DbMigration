049192653732c18dd56bd440c830d4bd111ead5f
Hal Vong <hvong@legalzoom.com>
2019-03-11 18:02:38 -0500

    read and writes file successful.Needs to set read and write permissions.

--- src/main.go
+++ src/main.go
@@ -7,16 +7,51 @@ import (
 	"strings"
 	"io/ioutil"
 	"strconv"
+	"path"
 	"path/filepath"
 )
+
+func regexfunc(infile string, outfile string) bool {
+
+	fmt.Printf("\nregexfunc:\n\tinfile: %v,\n\toutfile: %v\n",infile, outfile)
+
+	if _, err := os.Stat(infile); err == nil {
+
+		fmt.Printf("\ninfile: %v exists", infile)
+
+		r, err := ioutil.ReadFile(infile)//read file
+
+		if err != nil {
+			panic(err)
+			return false
+		}
+
+		//fmt.Println(r)
+
+		//deletes
+		content := strings.Replace(string(r), "CREATE DATABASE  IF NOT EXISTS `web_main_live`","", -1)
+		content = strings.Replace(content, "`web_main_live`","`web_main_qa`", -1)
+
+		err = ioutil.WriteFile(outfile, []byte(content), 0)//writes content
+
+		if err != nil {
+			panic(err)
+			return false
+		}
+		
+		return true
+	} 
+
+	return false 
+}
 
 func main() {
 
 	var files []string
-	//root := "/home/hal/dumps/Dump20190308"
-	root := "/home/hal/dumps/imports"
+	dir_reads := "/home/hal/dumps/reads"
+	dir_writes := "/home/hal/dumps/writes"
 
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+	err := filepath.Walk(dir_reads, func(path string, info os.FileInfo, err error) error {
         files = append(files, path)
         return nil
     })	
@@ -26,35 +61,36 @@ func main() {
 	}
 
 	idx := 1
-	for _, file := range files {
+	for _, infile := range files {
 
-		if _, err := os.Stat(file); err == nil {
+		if _, err := os.Stat(infile); err == nil { //checks if file exists
 
-			match, _ := regexp.MatchString("\\.sql$", file)
+			match, _ := regexp.MatchString("\\.sql$", infile)
 
-			if match { 
-				fmt.Println(strconv.Itoa(idx) + ". " + file) 
+			if match {//TODO
 
-				r, err := ioutil.ReadFile(file)
+				filepath, filename := path.Split(infile)
 
-				if err != nil {
-					panic(err)
-	            }
+				if filename == "web_main_live_attorney_lead.sql" {
 
-				fmt.Println(r)
+					outfile := dir_writes + "/" + filename  
 
-				content := strings.Replace(string(r), "CREATE DATABASE  IF NOT EXISTS `web_main_live`","", -1)
+					fmt.Printf("%v. file_name: %v, path: %v\n",strconv.Itoa(idx), filename, filepath)
+					var ok bool = regexfunc(infile, outfile)
 
-				err = ioutil.WriteFile(file, []byte(content), 0)
-				if err != nil {
-					panic(err)
+					if ok {
+						fmt.Println("\n\nSuccess. Writes file to",outfile)
+					} else {
+						fmt.Println("\nFailed")
+					}
+
 				}
 
 				idx += 1
 			}
 
 		} else if os.IsNotExist(err) {
-	        fmt.Println("Not found:" + file)
+	        fmt.Println("Not found:" + infile)
 		}
 
     }//for
